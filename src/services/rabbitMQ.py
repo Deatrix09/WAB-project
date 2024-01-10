5510fa99b5082fae7831c0ee060fdb02ed2c157b
Kleveta <xkleveta@mendelu.cz>
2024-01-10 14:11:58 +0100

    feat: rabbit working

--- src/services/rabbitMQ.py
+++ src/services/rabbitMQ.py
@@ -1,7 +1,11 @@
 import pika
+import os
 
 def send_player_created_message(player_data):
-    connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
+    rabbitmq_url = os.getenv("RABBITMQ_URL", "amqp://guest:guest@rabbitmq:5672/")
+    
+    params = pika.URLParameters(rabbitmq_url)
+    connection = pika.BlockingConnection(params)
     channel = connection.channel()
 
     channel.queue_declare(queue='player_created')
