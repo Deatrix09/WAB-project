a838aff9a128a8acc5159141a567ddcd1d2d03d9
Kleveta <xkleveta@mendelu.cz>
2024-01-10 14:18:55 +0100

    feat: final version

--- src/services/rabbitMQ.py
+++ src/services/rabbitMQ.py
@@ -3,15 +3,18 @@ import os
 
 def send_player_created_message(player_data):
     rabbitmq_url = os.getenv("RABBITMQ_URL", "amqp://guest:guest@rabbitmq:5672/")
-    
     params = pika.URLParameters(rabbitmq_url)
     connection = pika.BlockingConnection(params)
     channel = connection.channel()
 
     channel.queue_declare(queue='player_created')
 
+    # Předpokládáme, že player_data má atribut 'name'
+    message = f"New player registered - {player_data['name']}"
+
     channel.basic_publish(exchange='',
                           routing_key='player_created',
-                          body=str(player_data))
+                          body=message)
 
     connection.close()
+
